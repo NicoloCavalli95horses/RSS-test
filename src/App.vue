95e863a2326233fce62a710ceb76211bd3bb8223
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2025-12-11 10:46:24 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
 <template>
-  <article>
-    <header>header</header>
-    <h1>Title</h1>
-    <h2>Subtitle</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt mollitia modi ipsa rem veritatis commodi eligendi, labore enim, ea unde, suscipit nam delectus nemo velit ad. Iure adipisci at libero?</p>
-    <footer>footer</footer>
-
-  </article>
 </template>
 
 <script setup>
